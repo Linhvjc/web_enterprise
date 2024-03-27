df111115cef566ab0ca35df9d42bc7219645f4eb
ThienDuc1112 <ndthien1112@gmail.com>
2024-03-27 09:49:24 +0700

    fix admin role feature

--- Areas/Admin/Controllers/SemesterController.cs
+++ Areas/Admin/Controllers/SemesterController.cs
@@ -14,7 +14,6 @@ using WebEnterprise.Repositories.Abstraction;
 namespace WebEnterprise.Areas.Admin.Controllers
 {
     [Area("Admin")]
-
     [Authorize(Roles = "Admin")]
     public class SemesterController : Controller
     {
@@ -34,51 +33,11 @@ namespace WebEnterprise.Areas.Admin.Controllers
             return View(semesterList);
         }
 
-
         [HttpGet]
-
-    public class SemesterController : Controller
-    {
-        private readonly UniversityDbContext _context;
-
-        public SemesterController(UniversityDbContext context)
-        {
-            _context = context;
-        }
-
-        // GET: Admin/Semesters
-        public async Task<IActionResult> Index()
-        {
-              return _context.Semesters != null ? 
-                          View(await _context.Semesters.ToListAsync()) :
-                          Problem("Entity set 'UniversityDbContext.Semesters'  is null.");
-        }
-
-        // GET: Admin/Semesters/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null || _context.Semesters == null)
-            {
-                return NotFound();
-            }
-
-            var semester = await _context.Semesters
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (semester == null)
-            {
-                return NotFound();
-            }
-
-            return View(semester);
-        }
-
-        // GET: Admin/Semesters/Create
-
         public IActionResult Create()
         {
             return View();
         }
-
 
 
         [HttpPost]
@@ -130,79 +89,6 @@ namespace WebEnterprise.Areas.Admin.Controllers
         public IActionResult Delete(int? id)
         {
             if (id == null || id == 0)
-
-        // POST: Admin/Semesters/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Name,StartDate,EndDate,Id,CreatedBy,CreatedDate,LastModifiedBy,LastModifiedDate")] Semester semester)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(semester);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(semester);
-        }
-
-        // GET: Admin/Semesters/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null || _context.Semesters == null)
-            {
-                return NotFound();
-            }
-
-            var semester = await _context.Semesters.FindAsync(id);
-            if (semester == null)
-            {
-                return NotFound();
-            }
-            return View(semester);
-        }
-
-        // POST: Admin/Semesters/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Name,StartDate,EndDate,Id,CreatedBy,CreatedDate,LastModifiedBy,LastModifiedDate")] Semester semester)
-        {
-            if (id != semester.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(semester);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!SemesterExists(semester.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(semester);
-        }
-
-        // GET: Admin/Semesters/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.Semesters == null)
-
             {
                 return NotFound();
             }
@@ -251,43 +137,5 @@ namespace WebEnterprise.Areas.Admin.Controllers
         //    return Json(new { success = true, message = "Delete Successful" });
         //}
 
-        #endregion
-
-
-
-
-            var semester = await _context.Semesters
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (semester == null)
-            {
-                return NotFound();
-            }
-
-            return View(semester);
-        }
-
-        // POST: Admin/Semesters/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            if (_context.Semesters == null)
-            {
-                return Problem("Entity set 'UniversityDbContext.Semesters'  is null.");
-            }
-            var semester = await _context.Semesters.FindAsync(id);
-            if (semester != null)
-            {
-                _context.Semesters.Remove(semester);
-            }
-            
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool SemesterExists(int id)
-        {
-          return (_context.Semesters?.Any(e => e.Id == id)).GetValueOrDefault();
-        }
     }
 }
