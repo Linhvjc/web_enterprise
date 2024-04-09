0085cd28f508ba89de7c48f7f145bc19788048e3
Le Trung Tai <tailtgbd201817@fpt.edu.vn>
2024-04-10 02:05:12 +0700

    UI for displaying list magazine and contribution of role coordinator, make controller and UI for displaying contribution's detail

--- Areas/Coordinator/Controllers/MegazineController.cs
+++ Areas/Coordinator/Controllers/MegazineController.cs
@@ -1,12 +1,18 @@
 ﻿using AspNetCoreHero.ToastNotification.Abstractions;
 using AutoMapper;
+using DocumentFormat.OpenXml.Office2010.Excel;
+using Firebase.Auth;
+using Firebase.Storage;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
 using NuGet.Packaging.Signing;
+using System.Net.Sockets;
 using WebEnterprise.Infrastructure.Persistance;
 using WebEnterprise.Models.Entities;
 using WebEnterprise.Repositories.Abstraction;
+using WebEnterprise.Repositories.Implement;
 using WebEnterprise.ViewModels.Faculty;
 using WebEnterprise.ViewModels.Megazine;
 using WebEnterprise.ViewModels.Semester;
@@ -22,16 +28,20 @@ namespace WebEnterprise.Areas.Coordinator.Controllers
         private readonly IMapper _mapper;
         private readonly IHttpContextAccessor _httpContextAccessor;
 
-
         public INotyfService _notyfService { get; }
+
+        private static string apiKey = "AIzaSyDxbWg5cX5zoEDrQCssfBGh5CZrRkAr8ro";
+        private static string Bucket = "webenterprise-8a158.appspot.com";
+        private static string AuthEmail = "betngaongo@gmail.com";
+        private static string AuthPassword = "betngaongo";
 
         public MegazineController(UniversityDbContext context, IUnitOfWork unitOfWork, IMapper mapper, INotyfService notyfService, IHttpContextAccessor httpContextAccessor)
         {
             _context = context;
+            _httpContextAccessor = httpContextAccessor;
             _unitOfWork = unitOfWork;
             _mapper = mapper;
-            _notyfService = notyfService;
-            _httpContextAccessor = httpContextAccessor;
+            _notyfService = notyfService;      
         }
 
         // GET: Coordinator/Megazine
@@ -65,8 +75,8 @@ namespace WebEnterprise.Areas.Coordinator.Controllers
             var faculties = await _unitOfWork.FacultyRepository.GetAll();
             var semesters = await _unitOfWork.SemesterRepository.GetAll();
             // Assuming you have a SemesterRepository similar to FacultyRepository
-           var viewFacultyList = _mapper.Map<List<GetFacultyModel>>(faculties);
-           var viewSemesters= _mapper.Map<List<GetSemesterAdmin>>(semesters);
+            var viewFacultyList = _mapper.Map<List<GetFacultyModel>>(faculties);
+            var viewSemesters = _mapper.Map<List<GetSemesterAdmin>>(semesters);
             ViewBag.Faculties = new SelectList(viewFacultyList, "FacultyId", "Name");
             ViewBag.Semesters = new SelectList(viewSemesters, "SemesterId", "Name");
 
@@ -247,14 +257,6 @@ namespace WebEnterprise.Areas.Coordinator.Controllers
             return View(megazineModel);
         }
 
-
-
-
-
-
-
-
-
         [HttpGet]
         public async Task<IActionResult> Contributions(int id)
         {
@@ -280,6 +282,54 @@ namespace WebEnterprise.Areas.Coordinator.Controllers
             return View(megazine.Contributions);
         }
 
+        [HttpGet]
+        public async Task<IActionResult> ContributionDetail(int id)
+        {
+            var contribution = await _unitOfWork.ContributionRepository.GetContributionWithRelevant(id);
+            contribution.FacultyUserName = await _unitOfWork.UserRepository.FindUserName(contribution.FacultyUserName);
+            if (contribution == null)
+            {
+                return NotFound();
+            }
+
+            var auth = new FirebaseAuthProvider(new FirebaseConfig(apiKey));
+            var a = await auth.SignInWithEmailAndPasswordAsync(AuthEmail, AuthPassword);
+            var storage = new FirebaseStorage(Bucket, new FirebaseStorageOptions
+            {
+                AuthTokenAsyncFactory = () => Task.FromResult(a.FirebaseToken),
+            });
+            var reference = storage.Child("assets").Child(contribution.FilePath);
+            var listImagePath = new List<string>();
+
+            try
+            {
+                // Get a download URL for the file
+                var downloadUrlTask = reference.GetDownloadUrlAsync();
+                var downloadUrl = await downloadUrlTask;
+
+                if (downloadUrl != null)
+                {
+                    ViewBag.DocumentUrl = downloadUrl;
+                }
+
+                foreach (var image in contribution.imagePaths)
+                {
+                    var imageReference = storage.Child("assets").Child(image);
+                    var downloadImageTask = imageReference.GetDownloadUrlAsync();
+                    var imagePath = await downloadImageTask;
+                    listImagePath.Add(imagePath);
+                }
+                ViewBag.ImagePaths = listImagePath;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error retrieving download URL: " + ex.Message);
+            }
+
+            return View(contribution);
+        }
+
+
         [HttpPost]
         public async Task<IActionResult> ToggleContributionStatus(int id)
         {
@@ -296,10 +346,5 @@ namespace WebEnterprise.Areas.Coordinator.Controllers
 
             return RedirectToAction(nameof(Contributions), new { id = contribution.MegazineId }); // Quay trở lại danh sách Contributions với id của Megazine
         }
-
-
-
-
-
     }
 }
